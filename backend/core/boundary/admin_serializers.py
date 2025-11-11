ec17bfc10c18fb78a14a590c8a1d4b98e1f29c7d
Anish <164307407+AnishVIP03@users.noreply.github.com>
2025-11-11 19:45:43 +0800

    Update admin_serializers.py

--- backend/core/boundary/admin_serializers.py
+++ backend/core/boundary/admin_serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from core.models import FlaggedRequest, Request
 
+#This is for flagged req serializer 
 class FlaggedRequestSerializer(serializers.ModelSerializer):
     
     
